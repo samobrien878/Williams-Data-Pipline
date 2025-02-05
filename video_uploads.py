b8a650a532a50b0a6c136f676fdabaf8c52f2774
Joy Kissell <dzp6qm@virginia.edu>
2025-02-04 21:14:00 -0500

    dummy google drive connection

--- video_uploads.py
+++ video_uploads.py
@@ -4,4 +4,22 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 import time 
+
 folderLocation = r"C:\Users\\Pictures\Camera Roll"
+
+#Authenticating
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+
+if gauth.credentials is None:
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    gauth.Refresh()
+else:
+    gauth.Authorize()
+
+gauth.SaveCredentialsFile("mycreds.txt")    
+
+GoogleDrive = GoogleDrive(gauth)    
+
+#look into www.blomp.com for storage of videos
