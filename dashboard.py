e45114666a9df0cf6e4ee6fef7451b2f7949769f
Joy Kissell <dzp6qm@virginia.edu>
2025-02-04 21:45:23 -0500

    starting dash

---
+++ dashboard.py
@@ -0,0 +1,21 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+from pymongo import MongoClient
+
+#connect to MongoDB:
+MONGO_URI = MONGO_URI = "mongodb+srv://____:____@serverlessinstance0.gqqyx4s.mongodb.net/"
+DB_NAME = "training_data"
+COLLECTION_NAME = "metrics"
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+def get_data():
+    data = list(collection.find())
+    df = pd.DataFrame(data)
+    return df
+
